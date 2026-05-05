1dfaa648a47c65a21f5ac4ba1a62a8be2fa863ac
traorecheikh <atidiane741@gmail.com>
2026-05-05 22:29:10 +0000

    chore(apps/mobile-client/lib): update app_bottom_bar.dart

--- apps/mobile-client/lib/src/core/widgets/app_bottom_bar.dart
+++ apps/mobile-client/lib/src/core/widgets/app_bottom_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../theme/app_colors.dart';
+import 'package:beauteavenue_mobile_client/src/core/theme/app_theme.dart';
 
 /// Sticky bottom action bar with a surface background and an upward drop shadow.
 ///
@@ -12,25 +12,24 @@ class AppBottomBar extends StatelessWidget {
     required this.child,
     this.backgroundColor,
     this.padding,
+    this.topRadius,
     super.key,
   });
 
   final Widget child;
   final Color? backgroundColor;
   final EdgeInsets? padding;
+  final double? topRadius;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: backgroundColor ?? AppColors.surface,
-        boxShadow: [
-          BoxShadow(
-            color: AppColors.onSurface.withValues(alpha: 0.08),
-            blurRadius: 24,
-            offset: const Offset(0, -4),
-          ),
-        ],
+        borderRadius: topRadius != null
+            ? BorderRadius.vertical(top: Radius.circular(topRadius!))
+            : null,
+        boxShadow: AppShadows.nav,
       ),
       padding: padding ?? EdgeInsets.fromLTRB(20.w, 14.h, 20.w, 0),
       child: SafeArea(top: false, child: child),
