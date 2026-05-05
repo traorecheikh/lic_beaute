d6927d3cdc36ae80743dc95af8a5ca0969bdec9a
traorecheikh <atidiane741@gmail.com>
2026-05-05 22:29:07 +0000

    chore(apps/mobile-client/lib): update app_salon_list_items.dart

---
+++ apps/mobile-client/lib/src/core/widgets/app_salon_list_items.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../theme/app_theme.dart';
+import '../utils/app_list_utils.dart';
+
+class AppSalonListItems extends StatelessWidget {
+  const AppSalonListItems({
+    required this.items,
+    this.isStale = false,
+    this.cachedAt,
+    required this.itemBuilder,
+    super.key,
+  });
+
+  final List<dynamic> items;
+  final bool isStale;
+  final DateTime? cachedAt;
+  final Widget Function(BuildContext context, int index, dynamic item) itemBuilder;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.separated(
+      itemCount: AppListUtils.getItemCount(items.length, isStale, cachedAt),
+      separatorBuilder: (_, __) => gapH12,
+      itemBuilder: (context, i) => AppListUtils.buildItem(
+        context: context,
+        index: i,
+        items: items,
+        isStale: isStale,
+        cachedAt: cachedAt,
+        itemBuilder: itemBuilder,
+      ),
+    );
+  }
+}
