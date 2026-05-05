8c40eb3cb32f38a44c737417aa647508b8529c96
traorecheikh <atidiane741@gmail.com>
2026-05-05 22:29:09 +0000

    chore(apps/mobile-client/lib): update app_text_styles.dart

--- apps/mobile-client/lib/src/core/theme/app_text_styles.dart
+++ apps/mobile-client/lib/src/core/theme/app_text_styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'app_colors.dart';
+import 'package:beauteavenue_mobile_client/src/core/theme/app_theme.dart';
 
 abstract final class AppTextStyles {
   // ── Display — Cormorant Garamond (hero moments, salon names) ─────────────
