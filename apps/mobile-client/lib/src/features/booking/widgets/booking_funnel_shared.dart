f88f02cd9bcb2a3579dd3358cdb3b90361dcb15b
traorecheikh <atidiane741@gmail.com>
2026-05-05 22:29:07 +0000

    chore(apps/mobile-client/lib): update booking_funnel_shared.dart

---
+++ apps/mobile-client/lib/src/features/booking/widgets/booking_funnel_shared.dart
@@ -0,0 +1,16 @@
+export 'package:flutter/material.dart';
+export 'package:flutter_riverpod/flutter_riverpod.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:go_router/go_router.dart';
+
+export 'package:beauteavenue_mobile_client/src/core/theme/app_theme.dart';
+export '../../../core/widgets/app_booking_funnel_scaffold.dart';
+export '../../../core/widgets/app_bottom_bar.dart';
+export '../../../core/widgets/app_button.dart';
+export '../../../core/widgets/app_selectable_card.dart';
+export '../../../core/utils/app_haptics.dart';
+export '../../../core/widgets/app_snackbar.dart';
+export '../../../features/discovery/providers/salon_detail_provider.dart';
+export '../../../router/app_router.dart';
+export '../providers/booking_funnel_provider.dart';
+export '../widgets/funnel_step_bar.dart';
